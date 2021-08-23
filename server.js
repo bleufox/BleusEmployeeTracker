fc93db885bc071a90858168842a52ef7ab734456
bleufox <thebleufox8@gmail.com>
2021-08-23 02:09:22 -0700

    Everything is working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
+const divider = '--------------------------------------------------------------------'
 
 const db = mysql.createConnection(
     {
@@ -29,6 +30,10 @@ function mainMenu() {
                         value: "viewRoles"
                     },
                     {
+                        name: "View all Employees",
+                        value: "viewEmployees"
+                    },
+                    {
                         name: "Add a Department",
                         value: "newDepartment"
                     },
@@ -41,8 +46,8 @@ function mainMenu() {
                         value: "newEmployee"
                     },
                     {
-                        name: "Update Existing Employee Role",
-                        value: "existingRole"
+                        name: "Update an Existing Employee Role",
+                        value: "updateRole"
                     }
                 ]
             }
@@ -54,6 +59,9 @@ function mainMenu() {
                 case "viewRoles":
                     viewRoles();
                     break;
+                case "viewEmployees":
+                    viewEmployees();
+                    break;
                 case "newDepartment":
                     newDepartment();
                     break;
@@ -63,8 +71,8 @@ function mainMenu() {
                 case "newEmployee":
                     newEmployee();
                     break;
-                case "existingRole":
-                    existingRole();
+                case "updateRole":
+                    updateRole();
                     break;
                 default:
                     exit()
@@ -77,6 +85,7 @@ const viewDepartments = () => {
         if (err) {
             throw err
         } else {
+            console.log(divider)
             console.table(data);
             mainMenu();
         }
@@ -88,6 +97,19 @@ const viewRoles = () => {
         if (err) {
             throw err
         } else {
+            console.log(divider)
+            console.table(data);
+            mainMenu();
+        }
+    })
+}
+
+const viewEmployees = () => {
+    db.query(`SELECT * FROM employee`, (err, data) => {
+        if (err) {
+            throw err
+        } else {
+            console.log(divider)
             console.table(data);
             mainMenu();
         }
@@ -114,22 +136,199 @@ const newDepartment = () => {
 }
 
 const newRole = () => {
-    inquirer
-        .prompt({
-            name: 'role',
-            message: 'Please provide the title and salary of the new role.'
-        }).then(function (response) {
-            db.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, response.title, response.salary, response.department_id, (err) => {
-                if (err) {
-                    throw err
+    db.query(`SELECT * FROM department`, (err, result) => {
+        if (err) throw err
+        inquirer.prompt([
+            {
+                type: "input",
+                name: 'title',
+                message: 'Please provide the title.'
+            },
+            {
+                type: "input",
+                name: 'salary',
+                message: 'Please provide the salary.'
+            },
+            {
+                type: "list",
+                name: 'departmentName',
+                message: 'Please select the department name.',
+                choices() {
+                    const departmentArray = [];
+                    result.forEach(({ department_name }) => {
+                        departmentArray.push(department_name)
+                    })
+                    return departmentArray;
                 }
-                else {
-                    viewRoles();
-                    mainMenu();
+            },
+        ])
+            .then(response => {
+                db.query(`SELECT id FROM department WHERE department_name=?`, response.departmentName, (err, data) => {
+                    if (err) {
+                        throw err
+                    } else {
+                        let dep = JSON.stringify(data[0].id)
+                        db.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, [response.title, response.salary, dep], (err) => {
+                            if (err) {
+                                throw err
+                            } else {
+                                viewRoles();
+                                mainMenu();
+                            }
+                        }
+                        )
+                    }
                 }
+                )
             })
-        }
-,)
+    })
+}
+
+const newEmployee = () => {
+    db.query(`SELECT * FROM role`, (err, result) => {
+        if (err) throw err
+        inquirer.prompt([
+            {
+                type: "input",
+                name: 'firstName',
+                message: "Please provide the employee's first name."
+            },
+            {
+                type: "input",
+                name: 'lastName',
+                message: "Please provide the employee's last name."
+            },
+            {
+                type: "list",
+                name: 'title',
+                message: "Please select the new employee's title.",
+                choices() {
+                    const roleArray = [];
+                    result.forEach(({ title }) => {
+                        roleArray.push(title)
+                    })
+                    return roleArray;
+                }
+            },
+        ])
+            .then(response => {
+                db.query(`SELECT id FROM role WHERE title=?`, response.title, (err, results) => {
+                    if (err) {
+                        throw err
+                    } else {
+                        let title = JSON.stringify(results[0].id)
+                        db.query(`SELECT * FROM employee`, (err, data) => {
+                            if (err) throw err;
+                            inquirer.prompt([{
+                                type: 'list',
+                                name: "manager",
+                                message: "Select the new employee's manager.",
+                                choices() {
+                                    const managerArray = ['none'];
+                                    data.forEach(({ first_name, last_name }) => {
+                                        managerArray.push(`${first_name} ${last_name}`)
+                                    })
+                                    return managerArray;
+                                }
+                            }
+                            ])
+                                .then(answer => {
+                                    db.query(`SELECT id FROM employee WHERE CONCAT (first_name," ",last_name) = ?`, answer.manager, (err, data) => {
+                                        if (err) {
+                                            throw err
+                                        } else {
+                                            if (answer.manager !== 'none') {
+                                                manager = JSON.stringify(data[0].id)
+                                            } else {
+                                                manager = null
+                                            }
+                                        }
+                                        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [response.firstName, response.lastName, title, manager], (err) => {
+                                            if (err) {
+                                                throw err
+                                            } else {
+                                                viewEmployees();
+                                                mainMenu();
+                                            }
+                                        }
+                                        )
+                                    })
+                                })
+                        })
+                    }
+                }
+                )
+            })
+    })
+}
+
+const updateRole = () => {
+    db.query(`SELECT * FROM employee`, (err, result) => {
+        if (err) throw err
+        inquirer.prompt([
+            {
+                type: "list",
+                name: 'employee',
+                message: "Please select the employee you wish to change.",
+                choices() {
+                    const employeeArray = [];
+                    result.forEach(({ first_name, last_name }) => {
+                        employeeArray.push(`${first_name} ${last_name}`)
+                    })
+                    return employeeArray;
+                }
+            },
+        ])
+            .then(response => {
+                db.query(`SELECT id FROM employee WHERE CONCAT(first_name, ' ', last_name)=?`, response.employee, (err, results) => {
+                    if (err) {
+                        throw err
+                    } else {
+                        let employeeID = JSON.stringify(results[0].id)
+                        db.query(`SELECT * FROM role`, (err, data) => {
+                            if (err) throw err;
+                            inquirer.prompt([{
+                                type: 'list',
+                                name: "roleID",
+                                message: "What's the employee's new role?",
+                                choices() {
+                                    const roleArray = [];
+                                    data.forEach(({ title }) => {
+                                        roleArray.push(title)
+                                    })
+                                    return roleArray;
+                                }
+                            }
+                            ])
+                                .then(answer => {
+                                    db.query(`SELECT id FROM role WHERE title = ?`, answer.roleID, (err, data) => {
+                                        if (err) {
+                                            throw err
+                                        } else {
+                                            let roleIdentification = JSON.stringify(data[0].id)
+                                        db.query(`UPDATE employee SET role_id = ? WHERE id = ?`, [roleIdentification, employeeID], (err) => {
+                                        if (err) {
+                                            throw err
+                                        } else {
+                                            viewEmployees();
+                                            mainMenu();
+                                        }
+                                    }
+                                    )}
+                                    })
+                        })
+                    })
+            }
+                }
+    )
+})
+    })
+
+}
+
+const exit = () => {
+    console.log('Thanks for using the app')
+    process.exit();
 }
 
 mainMenu();
